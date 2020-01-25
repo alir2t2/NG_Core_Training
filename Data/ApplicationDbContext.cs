9f94ec0c1413116ae579edafd1970c700d11c263
Ali <amhussain7@gmail.com>
2020-01-24 23:23:01 -0800

    Adding necessary folders:

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NG_Core_Project.Data
+{
+    public class ApplicationDbContext
+    {
+    }
+}
